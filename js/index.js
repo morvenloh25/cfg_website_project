1a7d961b8a058190bcbbcb17c94fceba65ffa52e
Morven Loh <morvenjyloh@gmail.com>
2019-03-27 17:11:51 +0000

    Menu
    have changed nav bar
    things that need fixing:
    - javascript to enable drop down toggle
    - be able to size main/safety btn to same position/font-size as other strings on navbar

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 
+
 document.querySelector('.btn').addEventListener('click', function() {
   $('.content').toggleClass('show-main');
   $('.content').toggleClass('show-safety');
 });
+
+$('#myDropdown').on('show.bs.dropdown', function () {
+  // do something…
+})
